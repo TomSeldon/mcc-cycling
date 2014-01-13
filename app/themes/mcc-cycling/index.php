614a90ea4893baaf5dcfcd9f5f06746e9e3a994e
Tom Seldon <tom@tomseldon.co.uk>
2014-01-13 23:52:14 +0000

    Added styling for pager links

--- app/themes/mcc-cycling/index.php
+++ app/themes/mcc-cycling/index.php
@@ -17,7 +17,7 @@
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">
-    <ul class="pager">
+    <ul class="pager col-md-9 col-md-offset-3">
       <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
       <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
     </ul>
