6eb4b29ce1ecedca47d9906938ed85862322fe01
Tom Seldon <tom@tomseldon.co.uk>
2014-01-16 00:24:28 +0000

    Increased home page heading font weight at low screen sizes

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6a22060a420b30e40dcfb0b46d54f562');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4847ac7e9b3c6730501b55536607ef54');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
