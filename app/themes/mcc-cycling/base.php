f32828a575b23efe089008414c17d7b037e6a102
Tom Seldon <tom@tomseldon.co.uk>
2014-01-26 20:42:38 +0000

    Styling fixes

--- app/themes/mcc-cycling/base.php
+++ app/themes/mcc-cycling/base.php
@@ -10,38 +10,35 @@
   <?php
     do_action('get_header');
 
-    if (is_front_page()) {
+    // Use Bootstrap's navbar if enabled in config.php
+    if (current_theme_supports('bootstrap-top-navbar')) {
         get_template_part('templates/header-top-navbar');
     } else {
-        // Use Bootstrap's navbar if enabled in config.php
-        if (current_theme_supports('bootstrap-top-navbar')) {
-            get_template_part('templates/header-top-navbar');
-            get_template_part('templates/header-title');
-        } else {
-            get_template_part('templates/header');
-            get_template_part('templates/header-title');
-        }
+        get_template_part('templates/header');
     }
 
   ?>
 
   <div class="container" id="main-container">
 
-      <main class="main <?php if (roots_display_sidebar()): echo 'col-sm-8'; endif; ?>" role="main">
-          <?php
-              if (is_front_page()) {
-                  get_template_part('templates/header-title');
-              }
+      <?php get_template_part('templates/header-title'); ?>
 
+      <?php if (roots_display_sidebar()) : ?>
+      <div class="container">
+      <?php endif; ?>
+
+      <main class="main container <?php echo (roots_display_sidebar() ? 'col-sm-8' : ''); ?>" role="main">
+          <?php
               include roots_template_path();
           ?>
       </main><!-- /.main -->
 
-  <?php if (roots_display_sidebar()) : ?>
-      <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-      </aside><!-- /.sidebar -->
-  <?php endif; ?>
+      <?php if (roots_display_sidebar()) : ?>
+          <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+              <?php include roots_sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+      </div>
+      <?php endif; ?>
 
   </div>
 
