0e1d445d53f085e89c99252f5b0a375ef6eedfcf
Tom Seldon <tom@tomseldon.co.uk>
2014-02-02 22:52:24 +0000

    Added centre alignment to share text on sportive template

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e54c6df9c71ff5144aae5c72fb406957');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd2031b7cf7fa9fb5cf0ba3e451f280d0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
