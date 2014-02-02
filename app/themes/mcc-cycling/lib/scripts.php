b88b2a6d104547a9e612fc23ab1e8eefb3ff64e1
Tom Seldon <tom@tomseldon.co.uk>
2014-02-02 17:29:18 +0000

    Fixed form icon styling variations at low resolutions

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b322f7163b70c7bcb1bc09e78d0f8078');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5af318bed07159ac0641eafab643cbda');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
