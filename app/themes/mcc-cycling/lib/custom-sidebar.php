1a52f5c7e654266450da80537248bc2a8b91b997
Tom Seldon <tom@tomseldon.co.uk>
2014-02-03 01:26:04 +0000

    Added styling for thumbs cta

--- app/themes/mcc-cycling/lib/custom-sidebar.php
+++ app/themes/mcc-cycling/lib/custom-sidebar.php
@@ -17,9 +17,62 @@ function mcc_show_custom_sidebar()
                 case 'locations_cta':
                     mcc_sidebar_show_locations_layout( get_sub_field('locations') );
                     break;
+
+                case 'thumbnail_and_links':
+                    mcc_sidebar_show_thumb_and_link_layout();
+                    break;
             }
         }
     }
+}
+
+function mcc_sidebar_show_thumb_and_link_layout()
+{
+    echo '<div class="thumb-links cta">';
+    echo '  <h2>' . get_sub_field('heading') . '</h2>';
+
+
+    $thumbnail      = get_sub_field('thumbnail');
+    $thumb_alt      = $thumbnail['title'];
+    $thumb_src      = $thumbnail['sizes']['medium'];
+    $thumb_width    = $thumbnail['sizes']['medium-width'];
+    $thumb_height   = $thumbnail['sizes']['medium-height'];
+
+    echo "<img class='thumbnail' src='$thumb_src' alt='$thumb_alt' width='$thumb_width' height='$thumb_height' />'";
+
+    echo '<div class="intro-text">';
+        the_sub_field('intro_text');
+    echo '</div>';
+
+    echo '<ul class="links">';
+
+    while (has_sub_field('links')) {
+        $link_type  = get_sub_field('link_type');
+        $link_href  = false;
+        $link_label = get_sub_field('label');
+
+        switch ($link_type) {
+            case 'internal':
+                $linked_posts = get_sub_field('internal_link');
+
+                foreach ($linked_posts as $linked_post) {
+                    $link_href   = get_permalink($linked_post->ID);
+                    break;
+                }
+
+                break;
+
+            case 'external';
+                break;
+        }
+
+        if (false !== $link_href && false !== $link_label)
+            echo "<li><a href='$link_href'>$link_label<i class='read-more fa fa-chevron-right'></i></a></li>";
+    }
+
+    echo '</ul>';
+
+    echo '</div>';
 }
 
 function mcc_sidebar_show_locations_layout($locations)
--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ec4a0900fafaae5516eba267e9632ff7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '72e6bad392edbf93f92d83950d36d006');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
