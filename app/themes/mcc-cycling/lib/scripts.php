fa8bc40d76177322f1627fa85991a60a85ef5ddd
Tom Seldon <tom@tomseldon.co.uk>
2014-02-03 01:39:51 +0000

    Added some margin for sharing buttons

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bf4e9bc45018b750125599d1b1477cb1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'adac9f9e2e05cf4d14bd0a19272e2b93');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
