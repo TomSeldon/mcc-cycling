f7f335785afbc20af0d98df15c20d8de93b9c502
Tom Seldon <tom@tomseldon.co.uk>
2014-02-03 19:04:32 +0000

    Reduced side menu margin plus added scrolling

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '937def3b9d261f57efe17c2e1fbaa2a2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e57771869b947887095d361f8f174427');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
