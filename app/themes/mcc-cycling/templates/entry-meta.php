20f923f7d4ffde487f13d84c092da80e5f1d3a6d
Tom Seldon <tom@tomseldon.co.uk>
2014-02-03 18:56:24 +0000

    Removed author from articles

--- app/themes/mcc-cycling/templates/entry-meta.php
+++ app/themes/mcc-cycling/templates/entry-meta.php
@@ -1,4 +1,3 @@
 <p class="byline author vcard">
     <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>,
-    <?php echo __('by', 'mcc') . ' ' . get_the_author(); ?>
 </p>
