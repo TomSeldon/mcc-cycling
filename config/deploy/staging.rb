34e2f06158555d2f3fa7629cd8a3003ee1511284
Tom Seldon <tom@tomseldon.co.uk>
2014-02-06 08:14:06 +0000

    Set staging as live server- removed IP from staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,7 +8,7 @@ set :stage, 'staging'
 
 # Extended Server Syntax
 # ======================
-server '162.243.228.92', user: 'deploy', roles: %w{web app db}
+server '', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
