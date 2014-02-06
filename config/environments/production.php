50f12ee1ea74caa303fc38dd44c4fafd757e1f73
Tom Seldon <tom@tomseldon.co.uk>
2014-02-06 08:10:35 +0000

    Matched prod + staging envs

--- config/environments/production.php
+++ config/environments/production.php
@@ -11,4 +11,4 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-define( 'ACF_LITE' , true );
+define('ACF_LITE' , true );
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -11,5 +11,4 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-define('JETPACK_DEV_DEBUG', true);
-define( 'ACF_LITE' , true );
+define('ACF_LITE' , true );
