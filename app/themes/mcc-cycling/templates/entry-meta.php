723249b423f44c930c16f3b112a5554a5a720bf4
Tom Seldon <tom@tomseldon.co.uk>
2014-02-06 08:11:15 +0000

    Removed trailing comma from entry meta

--- app/themes/mcc-cycling/templates/entry-meta.php
+++ app/themes/mcc-cycling/templates/entry-meta.php
@@ -1,3 +1,3 @@
 <p class="byline author vcard">
-    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>,
+    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </p>
