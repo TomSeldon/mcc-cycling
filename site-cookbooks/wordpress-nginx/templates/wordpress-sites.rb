6e326bc9549899f9d1ae0327609580212c7edcc6
Tom Seldon <tom@tomseldon.co.uk>
2014-02-07 17:27:31 +0000

    Protected some private files

--- site-cookbooks/wordpress-nginx/templates/wordpress-sites.rb
+++ site-cookbooks/wordpress-nginx/templates/wordpress-sites.rb
@@ -1,22 +1,51 @@
 server {
-  listen       80;
-  server_name  <%= @host %>;
-  root         <%= @root %>;
-  access_log   <%= node['nginx']['log_dir'] %>/<%= @name %>.access.log;
-  error_log    <%= node['nginx']['log_dir'] %>/<%= @name %>.error.log;
+    listen       80;
+    server_name  <%= @host %>;
+    root         <%= @root %>;
+    access_log   <%= node['nginx']['log_dir'] %>/<%= @name %>.access.log;
+    error_log    <%= node['nginx']['log_dir'] %>/<%= @name %>.error.log;
 
-  # BEGIN W3TC Browser Cache
-  gzip on;
-  gzip_types text/css text/x-component application/x-javascript application/javascript text/javascript text/x-js text/richtext image/svg+xml text/plain text/xsd text/xsl text/xml image/x-icon;
-  # END W3TC Browser Cache
+    # Stop some private files being shown
+    location ~ /(config|Capfile|Gemfile(\.lock)?|composer(\.lock|\.json)|\.env) {
+      deny all;
+    }
 
-  # Site map rewrites
-  rewrite ^/sitemap_index\.xml$ /index.php?sitemap=1 last;
-  rewrite ^/([^/]+?)-sitemap([0-9]+)?\.xml$ /index.php?sitemap=$1&sitemap_n=$2 last;
+    # BEGIN W3TC Browser Cache
+    gzip on;
+    gzip_types text/css text/x-component application/x-javascript application/javascript text/javascript text/x-js text/richtext image/svg+xml text/plain text/xsd text/xsl text/xml image/x-icon;
+    location ~ \.(css|htc|js|js2|js3|js4)$ {
+        expires 31536000s;
+        add_header Pragma "public";
+        add_header Cache-Control "max-age=31536000, public";
+        add_header X-Powered-By "W3 Total Cache/0.9.3";
+    }
+    location ~ \.(html|htm|rtf|rtx|svg|svgz|txt|xsd|xsl|xml)$ {
+        expires 3600s;
+        add_header Pragma "public";
+        add_header Cache-Control "max-age=3600, public";
+        add_header X-Powered-By "W3 Total Cache/0.9.3";
+    }
+    location ~ \.(asf|asx|wax|wmv|wmx|avi|bmp|class|divx|doc|docx|eot|exe|gif|gz|gzip|ico|jpg|jpeg|jpe|json|mdb|mid|midi|mov|qt|mp3|m4a|mp4|m4v|mpeg|mpg|mpe|mpp|otf|odb|odc|odf|odg|odp|ods|odt|ogg|pdf|png|pot|pps|ppt|pptx|ra|ram|svg|svgz|swf|tar|tif|tiff|ttf|ttc|wav|wma|wri|xla|xls|xlsx|xlt|xlw|zip)$ {
+        expires 31536000s;
+        add_header Pragma "public";
+        add_header Cache-Control "max-age=31536000, public";
+        add_header X-Powered-By "W3 Total Cache/0.9.3";
+    }
+    # END W3TC Browser Cache
 
-  <% if @code -%>
-  <%= @code %>
-  <% end -%>
+    # BEGIN W3TC Minify core
+    rewrite ^/app/cache/minify.*/w3tc_rewrite_test$ /app/plugins/w3-total-cache/pub/minify.php?w3tc_rewrite_test=1 last;
+    rewrite ^/app/cache/minify/(.+/[X]+\.css)$ /app/plugins/w3-total-cache/pub/minify.php?test_file=$1 last;
+    rewrite ^/app/cache/minify/(.+\.(css|js))$ /app/plugins/w3-total-cache/pub/minify.php?file=$1 last;
+    # END W3TC Minify core
 
-  include <%= node['nginx']['dir'] %>/wordpress.conf;
+    # Site map rewrites
+    rewrite ^/sitemap_index\.xml$ /index.php?sitemap=1 last;
+    rewrite ^/([^/]+?)-sitemap([0-9]+)?\.xml$ /index.php?sitemap=$1&sitemap_n=$2 last;
+
+    <% if @code -%>
+    <%= @code %>
+    <% end -%>
+
+    include <%= node['nginx']['dir'] %>/wordpress.conf;
 }
