5b7a0707eb2a46ddb1e0ec80cd5c140f576741d3
Tom Seldon <tom@tomseldon.co.uk>
2014-02-08 11:01:08 +0000

    Moved AddThis buttons to separate template file

--- app/themes/mcc-cycling/templates/content-sportive.php
+++ app/themes/mcc-cycling/templates/content-sportive.php
@@ -128,36 +128,7 @@ while (have_posts()) : the_post(); ?>
                     </div>
                 <?php endif; ?>
 
-                <div class="share row">
-                    <h2>Share with a friend</h2>
-
-                    <!-- AddThis Button BEGIN -->
-                    <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-                        <a class="addthis_button_facebook">
-                            <i class="fa fa-facebook"></i>
-                            <span class="sr-only">Facebook</span>
-                        </a>
-                        <a class="addthis_button_twitter">
-                            <i class="fa fa-twitter"></i>
-                            <span class="sr-only">Twitter</span>
-                        </a>
-                        <a class="addthis_button_email">
-                            <i class="fa fa-envelope"></i>
-                            <span class="sr-only">E-mail</span>
-                        </a>
-                        <a class="addthis_button_print">
-                            <i class="fa fa-print"></i>
-                            <span class="sr-only">Print</span>
-                        </a>
-                        <a class="addthis_button_compact">
-                            <i class="fa fa-plus"></i>
-                            <span class="sr-only">More services</span>
-                        </a>
-                    </div>
-                    <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-                    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52e94e7e419949f1"></script>
-                    <!-- AddThis Button END -->
-                </div>
+                <?php get_template_part('templates/addthis', 'share'); ?>
             </div>
         </div>
     </article>
