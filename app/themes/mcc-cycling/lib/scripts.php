19a9d3c3833ec75598624a0fcec5c890c0899cf6
Tom Seldon <tom@tomseldon.co.uk>
2014-02-15 18:20:38 +0000

    Added link to view map

--- app/themes/mcc-cycling/lib/scripts.php
+++ app/themes/mcc-cycling/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('animate.css', get_template_directory_uri() . '/assets/css/animate.min.css', false, '794cb380366e3fd8ea9aaec75fe9a5ab');
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '30a202ceefcf5215faa7679e7ebd1dec');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2bdb42ae6da4b797e8cc670c0261d10d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- app/themes/mcc-cycling/templates/archive-locations.php
+++ app/themes/mcc-cycling/templates/archive-locations.php
@@ -20,6 +20,12 @@
     $locations = mcc_get_locations(get_post_type());
 ?>
 
+<?php if (count($locations)): ?>
+<div class="view-on-map">
+    <a href="#map-container" class="redButton">View all on map</a>
+</div>
+<?php endif; ?>
+
 <div class="pagination">
     <?php echo $pagination; ?>
 </div>
@@ -35,34 +41,36 @@
 </div>
 
 <?php if (count($locations)): ?>
-    <h2>Map of locations</h2>
-    <div id="map-locations" class="map" data-map="" data-options="locationsMapSettings">
-        <?php foreach ($locations as $location): ?>
-            <div class="marker sr-only"
-                 data-marker=""
-                 data-lng="<?php echo $location['lng']; ?>"
-                 data-lat="<?php echo $location['lat']; ?>"
-                 data-address="<?php echo $location['address']; ?>"
-                 data-title="<?php echo $location['post']->post_title; ?>">
+    <div id="map-container">
+        <h2>Map of locations</h2>
+        <div id="map-locations" class="map" data-map="" data-options="locationsMapSettings">
+            <?php foreach ($locations as $location): ?>
+                <div class="marker sr-only"
+                     data-marker=""
+                     data-lng="<?php echo $location['lng']; ?>"
+                     data-lat="<?php echo $location['lat']; ?>"
+                     data-address="<?php echo $location['address']; ?>"
+                     data-title="<?php echo $location['post']->post_title; ?>">
 
-                <div class="entry">
-                    <?php if (has_post_thumbnail($location['post']->ID)): ?>
-                        <div class="location-thumb pull-left">
-                            <a href="<?php echo get_permalink($location['post']->ID); ?>">
-                                <?php echo get_the_post_thumbnail($location['post']->ID, 'thumbnail', array('class' => 'thumbnail')); ?>
-                            </a>
+                    <div class="entry">
+                        <?php if (has_post_thumbnail($location['post']->ID)): ?>
+                            <div class="location-thumb pull-left">
+                                <a href="<?php echo get_permalink($location['post']->ID); ?>">
+                                    <?php echo get_the_post_thumbnail($location['post']->ID, 'thumbnail', array('class' => 'thumbnail')); ?>
+                                </a>
+                            </div>
+                        <?php endif; ?>
+
+                        <div class="title pull-left">
+                            <h4><?php echo $location['post']->post_title; ?></h4>
                         </div>
-                    <?php endif; ?>
+                    </div>
 
-                    <div class="title pull-left">
-                        <h4><?php echo $location['post']->post_title; ?></h4>
+                    <div class="info pull-left">
+                        <p><a href="<?php echo get_permalink($location['post']->ID); ?>">Click here</a> for more information.</p>
                     </div>
                 </div>
-
-                <div class="info pull-left">
-                    <p><a href="<?php echo get_permalink($location['post']->ID); ?>">Click here</a> for more information.</p>
-                </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
 <?php endif; ?>
