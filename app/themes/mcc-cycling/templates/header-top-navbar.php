3687202f7935d7d9486e2054e68b8fc81c749e74
Tom Seldon <tom@tomseldon.co.uk>
2014-03-04 20:24:19 +0000

    Updated cyclist bg and fixed styles

--- app/themes/mcc-cycling/templates/header-top-navbar.php
+++ app/themes/mcc-cycling/templates/header-top-navbar.php
@@ -2,19 +2,4 @@
 <!-- header -->
 <div class="collapse navbar-side-collapse" role="navigation">
     <?php dynamic_sidebar('main-menu'); ?>
-
-    <div class="row social">
-        <div class="col-xs-1">
-            <span class="sr-only">Facebook</span>
-            <span class="icon-facebook"></span>
-        </div>
-        <div class="col-xs-1">
-            <span class="sr-only">Twitter</span>
-            <span class=""></span>
-        </div>
-        <div class="col-xs-1">
-            <span class="sr-only">Google Plus</span>
-            <span class=""></span>
-        </div>
-    </div>
 </div>
