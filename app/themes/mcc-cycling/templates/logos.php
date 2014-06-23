72aaed6c4a0c0e26797f3f6ab232e669783ae757
Tom Seldon <tom@tomseldon.co.uk>
2014-06-23 23:04:22 +0100

    Changed logos

--- app/themes/mcc-cycling/templates/logos.php
+++ app/themes/mcc-cycling/templates/logos.php
@@ -1,14 +1,5 @@
 <div class="row logos">
-    <div class="col-xs-6 col-sm-3 col-md-2 col-md-offset-2 logo" role="navigation">
-        <a href="http://www.monmouthshire.gov.uk/">
-            <img
-                src="/app/themes/mcc-cycling/assets/img/monmouthshire-cc-logo.png"
-                width="165"
-                height="37"
-                alt="Monmouthshire County Council" />
-        </a>
-    </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 logo" role="navigation">
+    <div class="col-xs-6 col-sm-4 col-md-4 logo" role="navigation">
         <a href="http://www.britishcycling.org.uk/">
             <img
                 src="/app/themes/mcc-cycling/assets/img/british_cycling.png"
@@ -17,22 +8,21 @@
                 alt="British Cycling" />
         </a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 logo" role="navigation">
-        <a href="http://monleisure.co.uk/">
+    <div class="col-xs-6 col-sm-4 col-md-4 logo" role="navigation">
+        <a href="http://www.monmouthshire.gov.uk/">
             <img
-                src="/app/themes/mcc-cycling/assets/img/monmouthshire-leisure-logo.png"
-                width="130"
-                height="42"
-                alt="Monmouthshire Leisure" />
+                src="/app/themes/mcc-cycling/assets/img/monmouthshire-cc-logo.png"
+                width="165"
+                height="37"
+                alt="Monmouthshire County Council" />
         </a>
     </div>
-    <div class="col-xs-6 col-sm-3 col-md-2 logo" role="navigation">
-        <a href="http://www.britishcycling.org.uk/nationalroadchampionships">
-            <img
-                src="/app/themes/mcc-cycling/assets/img/national-champ-logo.png"
-                width="160"
-                height="39"
-                alt="National Championships" />
-        </a>
+
+    <div class="col-xs-12 col-sm-3 col-md-3 logo" role="navigation">
+        <img
+            src="/app/themes/mcc-cycling/assets/img/nas-logo.png"
+            width="60"
+            height="42"
+            alt="Northern Automotive Systems" />
     </div>
 </div>
